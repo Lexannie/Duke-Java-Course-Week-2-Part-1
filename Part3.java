63d9d1a4767dda47a58ff1750b8985a51f6e0c7c
Lexannie <159739599+Lexannie@users.noreply.github.com>
2024-03-06 13:34:42 -0500

    Add files via upload
    add method lastPart to class Part3

--- Part3.java
+++ Part3.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.time.LocalTime;
 
 public class Part3 {
-    boolean twoOccurrences(String stringa, String stringb){
+    public boolean twoOccurrences(String stringa, String stringb){
         int firstFoundAt;
         int secondFoundAt;       
         // check for first occurrence of stringa in stringb from beginning
@@ -26,49 +26,89 @@ public class Part3 {
         return false;
     }
     
-    void testing(){
+    /**
+     * lastPart returns part of stringb that follows stringa 
+     * if stringa not found returns stringb
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public String lastPart(String stringa, String stringb) {
+        int firstFoundAt;
+        String answer = stringb;
+        
+        firstFoundAt = stringb.indexOf(stringa, 0);
+        if (firstFoundAt >= 0){
+            answer  = stringb.substring(firstFoundAt + 2);
+        }
+        
+        return answer;
+    }
+
+    /**
+     * testing contains test cases wwith calls to test twoOccurrences and lastPart
+     *
+     * @param  none
+     * @return void
+     */
+    public void testing(){
         boolean isFound;
+        String answerLastPart;
         String string1;
         String string2;
+        
         LocalTime time = LocalTime.now();
-        
         System.out.println("testing of class Part3 ****** " + time);
-        
-        string1 = "needle";
+
+        string1 = "an";
+        string2 = "anana try this one";
+        answerLastPart = lastPart(string1, string2);
+        System.out.println("   part of " + string2 +" that follows " + string1 + " is: " + answerLastPart);
+
+        string1 = "an";
+        string2 = "banana";
+        answerLastPart = lastPart(string1, string2);
+        System.out.println("   part of " + string2 +" that follows " + string1 + " is: " + answerLastPart);
+
+        string1 = "zoo";
+        string2 = "forest";
+        answerLastPart = lastPart(string1, string2);
+        System.out.println("   part of " + string2 +" that follows " + string1 + " is: " + answerLastPart);
+
         string2 = "haystack";
         isFound = twoOccurrences(string1, string2);
-        System.out.println("   " + string1 +" found twice in " + string2 + " is: " +isFound);
-        
+        System.out.println("   " + string1 +" found twice in " + string2 + " is: " + isFound);
+
         string1 = "b";
         string2 = "bbye";
         isFound = twoOccurrences(string1, string2);
-        System.out.println("   " + string1 +" found twice in " + string2 + " is: " +isFound);
-        
+        System.out.println("   " + string1 +" found twice in " + string2 + " is: " + isFound);
+
         string1 = "by";
         string2 = "A story by Abby Long";
         isFound = twoOccurrences(string1, string2);
-        System.out.println("   " + string1 +" found twice in " + string2 + " is: " +isFound);
-        
+        System.out.println("   " + string1 +" found twice in " + string2 + " is: " + isFound);
+
         string1 = "a";
         string2 = "banana";
         isFound = twoOccurrences(string1, string2);
-        System.out.println("   " + string1 +" found twice in " + string2 + " is: " +isFound);
+        System.out.println("   " + string1 +" found twice in " + string2 + " is: " + isFound);
 
         string1 = "atg";
         string2 = "ctgtatgta";
         isFound = twoOccurrences(string1, string2);
-        System.out.println("   " + string1 +" found twice in " + string2 + " is: " +isFound);
-         
+        System.out.println("   " + string1 +" found twice in " + string2 + " is: " + isFound);
+
     }
-    
+
     public static void main (String[] args) {
         LocalTime time = LocalTime.now();
         Part3 part3Runner = new Part3();
-       
+
         // print out blank line and header for each iteration of this test
         System.out.println("  ");
         System.out.println("main of class Part3 ****** " + time);
-        
+
         // Run test cases for findSimpleGene
         part3Runner.testing();
     }
